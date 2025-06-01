3f1a01df1dd3d619002afb57a1a5d208a53f699e
FANGAPELE COULIBALY <coulfang@gmail.com>
2025-06-01 14:17:51 +0000

    Add files via upload
    Signed-off-by: FANGAPELE COULIBALY <coulfang@gmail.com>

--- MainActivity.java
+++ MainActivity.java
@@ -3,15 +3,10 @@ package com.coulfang;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        TextView hello = new TextView(this);
-        hello.setText("Bienvenue dans CoulFang !");
-        setContentView(hello);
     }
 }
